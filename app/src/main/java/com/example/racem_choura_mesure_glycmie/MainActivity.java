9ab51180583f89d1fe60adeb63fdcc06c94f04b7
racemchoura <racem820@gmail.com>
2023-10-23 00:53:03 +0200

    Final commit

---
+++ app/src/main/java/com/example/racem_choura_mesure_glycmie/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.racem_choura_mesure_glycmie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
